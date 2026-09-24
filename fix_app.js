ca67c1b564b9f9e91a4ddcb5e623cdc5613ef0d8
developmentplandeviaje-blip <developmentplandeviaje@gmail.com>
2026-09-24 11:56:19 -0400

    chore: remove temporary fix_app.js

--- fix_app.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('react-frontend/src/App.jsx', 'utf8');
-
-code = code.replace(/useState\('servicios_hoteles'\)/g, "useState('dashboard')");
-code = code.replace(/setActiveRoute\('servicios_hoteles'\);/g, "setActiveRoute('dashboard');");
-
-fs.writeFileSync('react-frontend/src/App.jsx', code, 'utf8');
-console.log("App.jsx fixed");
